bfcd90d22c476bcf1fc3145101e4be0d86fd2e55
Santiago Botero Correa <sabotero@gmail.com>
2017-12-02 12:20:43 +0100

    structs, enums, patter matching

---
+++ enums/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let coin1 = Coin::Quarter(UsState::Alabama);
+    let value1 = value_in_cents(coin1);
+    println!("value is {}", value1);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("five is {:?}, six is {:?}, none is {:?}", five, six, none);
+
+    let coin = Coin::Quarter(UsState::Alaska);
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+}
+
+#[derive(Debug)] // So we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... etc
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
