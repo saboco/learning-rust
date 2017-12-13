7b09457b17a312051d134d49d9899b06b738ee81
saboco <sabotero@gmail.com>
2017-12-13 14:48:34 +0100

    Drop

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -27,7 +27,26 @@ fn main(){
     let m = MyBox::new(String::from("Rust"));
     println!("m is {:?}", m);
     hello(&m);
+
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.");
+
+    let e = CustomSmartPointer { data: String::from("some data") };
+    println!("CustomSmartPointer created.");
+    drop(e);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
 fn hello(name: &str) {
     println!("Hello, {}!", name);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
 }
