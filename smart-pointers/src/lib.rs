ca73c2dca374c37ed5171acf1451eb59f618ae6b
saboco <sabotero@gmail.com>
2018-01-05 10:55:11 +0100

    Reference counting

--- smart-pointers/src/lib.rs
+++ smart-pointers/src/lib.rs
@@ -1,15 +1,20 @@
 pub mod mybox;
 pub use mybox::*;
+use std::rc::Rc;
 
 pub enum MyList {
     Cons(i32, Box<MyList>),
     Nil,
 }
 
+pub enum MyListRc {
+    Cons(i32, Rc<MyListRc>),
+    Nil,
+}
+
 pub fn cons(a: i32, b:i32) -> MyList {
     MyList::Cons(a, Box::new(MyList::Cons(b, Box::new(MyList::Nil))))
 }
-
 
 #[cfg(test)]
 mod tests {
--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,5 +1,7 @@
 extern crate smart_pointers;
 use smart_pointers::MyBox;
+use smart_pointers::MyListRc::{Cons, Nil};
+use std::rc::Rc;
 
 fn main(){
     let x = 5;
@@ -36,7 +38,24 @@ fn main(){
     println!("CustomSmartPointer created.");
     drop(e);
     println!("CustomSmartPointer dropped before the end of main.");
+
+    // shared list with Rc
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
+
+    // visualizing the counting
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
+
 fn hello(name: &str) {
     println!("Hello, {}!", name);
 }
