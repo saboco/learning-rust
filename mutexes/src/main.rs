1fe8b360871c3ef435aaea7ac13c8019586be18d
saboco <sabotero@gmail.com>
2018-01-05 17:25:13 +0100

    mutexes

---
+++ mutexes/src/main.rs
@@ -0,0 +1,41 @@
+use std::sync::{Mutex, Arc};
+use std::thread;
+
+fn main() {
+    println!("--- Single thread ---");
+    single_thread();
+    println!("--- Sharing mutex ---");
+    sharing_mutex();
+}
+
+fn single_thread(){
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
+
+fn sharing_mutex(){
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
